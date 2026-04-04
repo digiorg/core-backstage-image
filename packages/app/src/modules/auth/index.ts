c990107e136e37ced4735c9db8e9e95dce0c13b7
Simon (it-stud.io) <simon@it-stud.io>
2026-04-04 20:26:06 +0000

    fix(auth): Create oidcAuthApiRef locally
    The oidcAuthApiRef is not exported from @backstage/core-plugin-api,
    so we need to create it ourselves with the correct API interface.

--- packages/app/src/modules/auth/index.ts
+++ packages/app/src/modules/auth/index.ts
@@ -4,8 +4,23 @@ import {
 } from '@backstage/plugin-app-react';
 import { SignInPage } from '@backstage/core-components';
 import { createFrontendModule } from '@backstage/frontend-plugin-api';
-import { oidcAuthApiRef } from '@backstage/core-plugin-api';
+import {
+  createApiRef,
+  type OpenIdConnectApi,
+  type ProfileInfoApi,
+  type BackstageIdentityApi,
+  type SessionApi,
+} from '@backstage/core-plugin-api';
 import React from 'react';
+
+/**
+ * API reference for OIDC authentication
+ */
+export const oidcAuthApiRef = createApiRef<
+  OpenIdConnectApi & ProfileInfoApi & BackstageIdentityApi & SessionApi
+>({
+  id: 'internal.auth.oidc',
+});
 
 /**
  * Custom Sign-In Page with Guest and OIDC (Keycloak) providers
